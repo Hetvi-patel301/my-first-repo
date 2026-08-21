688151ec00d6dcfce57f4e2f66433ab1d8f948e6
Hetvi Patel <hetvip301@gmail.com>
2026-08-21 17:37:18 +0530

    Fix goodbye message formatting in FundamentalBooster.py

---
+++ project_1/FundamentalBooster.py
@@ -0,0 +1,43 @@
+print("welcome to the Interactive Personal Data Collector!")
+name = str(input("Please Enter Your Name:"))
+age = int(input("Please Enter Your Age:"))
+height = float(input("Please Enter Your Height in meters:"))
+f_num = int(input("Please Enter Your Favourite Number:"))
+c_year =int(input("Enter current Year:"))
+print("\n")
+
+birth = c_year-age
+
+print("Thank You! Here is the information we collected")
+print("\n")
+print("Name:",name,"(Type:",type(name),",","Memory Address:",id(name),")")
+print("Age:",age,"Type:",type(age),",","Memory Address:",id(age),")")
+print("Height:",height,"(Type:",type(height),",","Memory Address:",id(height),")")
+print("Favourite Number:",f_num,"(Type:",type(f_num),",","Memory Address:",id(f_num),")")
+print("\n")
+
+print("Your Birth Year is Approximetly:",birth,"(based on your age of ",age,")")
+print("\n")
+print("Thank You for using Personal Data Collector. Goodbye!")
+=======
+print("wlcome to the Interactive Personal Data Collector!")
+name = str(input("Please Enter Your Name:"))
+age = int(input("Please Enter Your Age:"))
+height = float(input("Please Enter Your Height in meters:"))
+f_num = int(input("Please Enter Your Favourite Number:"))
+c_year =int(input("Enter current Year:"))
+print("\n")
+
+birth = c_year-age
+
+print("Thank You! Here is the information we collected")
+print("\n")
+print("Name:",name,"(Type:",type(name),",","Memory Address:",id(name),")")
+print("Age:",age,"Type:",type(age),",","Memory Address:",id(age),")")
+print("Height:",height,"(Type:",type(height),",","Memory Address:",id(height),")")
+print("Favourite Number:",f_num,"(Type:",type(f_num),",","Memory Address:",id(f_num),")")
+print("\n")
+
+print("Your Birth Year is Approximetly:",birth,"(based on your age of ",age,")")
+print("\n")
+print("Thank You for using Personal Data Collector. Goodbye!")
